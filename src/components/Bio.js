0137fb2b969c4776cd0632ea071f8e5ff68c6b16
Alan Gou <alanjgou@gmail.com>
2017-08-05 17:34:46 -0400

    Initial commit
    Built with gatsby-starter-blog.

---
+++ src/components/Bio.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <p
+        style={{
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        {/* <img
+          src={profilePic}
+          alt={`Kyle Mathews`}
+          style={{
+            float: 'left',
+            marginRight: rhythm(1 / 4),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        /> */}
+      </p>
+    )
+  }
+}
+
+export default Bio
